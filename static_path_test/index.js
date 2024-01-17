92fe141730d5f92b8067eead9511026ca0de64fd
sspdabeen <dabeenp22@sensing.co.kr>
2024-01-17 10:17:27 +0900

    static file send to client

--- static_path_test/index.js
+++ static_path_test/index.js
@@ -1,19 +1,54 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = 4000;
+const fs = require("fs");
+const { sanitize } = require("sanitize-filename"); // FilePath Custom
 
-const imgDirectory = "../../img";
-
-app.use("/test", express.static("../img/"));
+/**
+ * Run serve
+ * npx nodemon index.js
+ *  */
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// 1. Static folder Access
+app.use("/test", express.static("./public"));
+// How to use
+// localhost:4000/test/filename.png
+
+// 2. Local Video file Send to Client
+app.get("/videofile/:filename", async (req, res) => {
+  const { filename } = req.params;
+  // const filePath = path.join(__dirname, "test", filename);
+  const filePath = path.join(__dirname, "../public", filename);
+  console.log("filePath", filePath);
+  const getFileStatSize = fs.stat(filePath, (err, stats) => {
+    if (err) {
+      console.log(err.message);
+      throw err;
+    }
+    console.log("stat", stats.size);
+    return stats.size;
+  });
+  try {
+    const fileStream = await fs.createReadStream(filePath);
+    res.set({
+      "Content-Type": "video/mp4",
+      "Content-Length": getFileStatSize, // file size setting
+    });
+    // 2-1 __ with filestream ___ unable to control video files on the client side. close to the stream method.
+    // fileStream.pipe(res);
+    // 2-2 __ with sendFile __ Just send VideoFile to client side.
+    res.status(200).sendFile(filePath);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-// npx nodemon index.js
-// 4728d6a6-da10-4349-8e6d-76683f7b5086
